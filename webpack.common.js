1e1dfcc7538f9e2b089a245b06b86ad0bfe9e5c5
MantleBee <mantlebee@outlook.com>
2019-08-05 21:18:03 +0200

    Predisposizione al rilascio su npmjs, piccoli interventi sul template

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,18 @@
 const path = require("path");
 
 // Configuration
-const mode = "development";
 const entry = {
-  app: "./src/index.ts"
+  tcl: "./src/index.ts"
 };
-const filename = "[name].bundle.js";
-const outdir = "dist";
+const outdir = "./dist";
 
 // Module
 module.exports = {
-  mode,
   entry,
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: outdir
-  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
       }
@@ -29,7 +22,6 @@ module.exports = {
     extensions: [".ts"]
   },
   output: {
-    filename,
     path: path.resolve(__dirname, outdir)
   }
 };
